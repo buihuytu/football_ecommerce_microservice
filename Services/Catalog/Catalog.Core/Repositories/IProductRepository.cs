57e4decbf6908cd1762f3948cf4a3ec10c9ab816
buihuytu <bhtu1234@gmail.com>
2025-06-14 12:23:35 +0700

    first commit

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+
+        Task<Product> GetProductById(string id);
+
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+
+        Task<IEnumerable<Product>> GetProductsByType(string typeId);
+
+        Task<IEnumerable<Product>> GetProductsByBrand(string brandId);
+
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
